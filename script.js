52e3c2e61dca8ccc8f1ac745e3ce30fc735952ce
Dar <60307149@udst.edu.qa>
2026-02-12 14:08:42 +0300

    Fix lives counter to correctly display remaining lives (fixes #9)

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ function updateWrongLetters() {
 }
 
 function updateLives() {
-    const livesLeft = gameState.maxWrong - gameState.wrongGuesses + 1;
+    const livesLeft = gameState.maxWrong - gameState.wrongGuesses;
     document.getElementById('livesLeft').textContent = livesLeft;
 }
 
